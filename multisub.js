221eb791a39434b43b7ad86836dff4b49a041724
brapifra <brapifra@hotmail.com>
2016-06-25 18:00:40 +0200

    Minor fixes and CSS implementation

--- multisub.js
+++ multisub.js
@@ -1,57 +1,62 @@
 /**
- * @author brais
+ * @author brapifra
  */
-window.onload = function() {
-	var videos = document.querySelectorAll("video[data-subing]");
-	for (var i = 0; i < videos.length; i++) {
-		var video = videos[i];
-		for (var k = 0; k < video.textTracks.length; k++) {
-			video.textTracks[k].mode = "hidden";
-		}
-		var start = function() {
-			updateCues(video);
-		};
-		video.addEventListener("play", start);
-	}
-	function updateCues(video) {
-		var track;
-		var cues;
-		video.pause();
-		tracklist = video.textTracks;
-		for (var j = 0; j < tracklist.length; j++) {
-			track = tracklist[j];
-			if ((j + 1) % 2 == 0) {
-				cues = track.cues;
-				for (var k = 0; k < cues.length; k++) {
-					cues[k].line = 0;
-					cues[k].text = "<c.red>" + cues[k].text + "</c>";
-				}
-			}
-			track.mode = "showing";
-		}
-		video.removeEventListener("play", start);
-		video.addEventListener('contextmenu', function(ev) {
-			ev.preventDefault();
-			hideshowCues(video);
-		});
-		video.play();
-	};
-	function hideshowCues(video) {
-		var track;
-		var cues;
-		tracklist = video.textTracks;
-		for (var j = 0; j < tracklist.length; j++) {
-			if (j == 0) {
-				continue;
-			}
-			track = tracklist[j];
-			if (track.mode == "showing") {
-				track.mode = "hidden";
-			} else {
-				video.pause();
-				track.mode = "showing";
-			}
-		}
-	}
+(function () {
+    var videos;
+    var video;
+    var tracklist;
+    var track;
+    var cues;
+    var colors = [];
+    window.onload = function () {
+        videos = document.querySelectorAll("video[data-multisub]");
+        for (var i = 0; i < videos.length; i++) {
+            video = videos[i];
+            colors = video.dataset.multisub.split(",");
+            for (var j = 0; j < video.textTracks.length; j++) {
+                video.textTracks[j].mode = "hidden";
+            }
+            var start = function () {
+                updateCues();
+            };
+            video.addEventListener("play", start);
+        }
+        function updateCues() {
+            video.pause();
+            tracklist = video.textTracks;
+            for (var i = 0; i < tracklist.length; i++) {
+                track = tracklist[i];
+                cues = track.cues;
+                for (var j = 0; j < cues.length; j++) {
+                    if ((i + 1) % 2 == 0) {
+                        cues[j].line = 1;
+                    }
+                    cues[j].text = "<c." + colors[i] + ">" + cues[j].text + "</c.>";
+                }
+                track.mode = "showing";
+            }
+            video.removeEventListener("play", start);
+            video.addEventListener('contextmenu', function (ev) {
+                ev.preventDefault();
+                hideshowCues();
+            });
+            video.play();
+        };
+        function hideshowCues() {
+            tracklist = video.textTracks;
+            for (var i = 0; i < tracklist.length; i++) {
+                if (i == 0) {
+                    continue;
+                }
+                track = tracklist[ij];
+                if (track.mode == "showing") {
+                    track.mode = "hidden";
+                } else {
+                    video.pause();
+                    track.mode = "showing";
+                }
+            }
+        }
 
-};
+    };
+})();
