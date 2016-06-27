596fb48433f77b9f0a1ce0dbbcf9b935fb7c3a6a
brapifra <brapifra@hotmail.com>
2016-06-27 17:48:22 +0200

    Huge changes: max subtitles=2, drag srt or vtt files...

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # MultiSub
-Javascript library for watching two or more subtitles at the same time in any HTML5 video. Ideally created for showing two subtitles, one
+Javascript library for watching two (more could be possible but the maximum is blocked to 2 for now) subtitles at the same time in any HTML5 video. Ideally created for showing two subtitles, one
 in a foreign language, and another in native language in case that you had understanding context problems.
 
 You just need to add the "data-multisub" attribute to any html5 video with tracks:
 ```Html
-<video width="500" height="400" data-multisub controls>
-            <source src=http://techslides.com/demos/sample-videos/small.mp4 type=video/mp4>
-            <track kind="subtitles" src="sub.vtt">
-            <track kind="subtitles" src="sub2.vtt">
-        </video>
+<video width="500" height="400" data-multisub="pink,yellow" controls>
+    <source src=http://techslides.com/demos/sample-videos/small.mp4 type=video/mp4>
+    <track kind="subtitles" src="../sub.vtt">
+    <track kind="subtitles" src="../sub2.vtt">
+    <track kind="subtitles" src="../sub2.vtt"><!--This subtitle won't be load (max=2)-->
+</video>
   ```
 Just add the <b>name of the css class</b> separated by commas in the attribute for css customization:
 ```Html
@@ -18,5 +19,6 @@ The css class will be applied to the subtitles in order, first class (yellow in 
 The multisub.css file can be edited, so anyone can create their custom css classes.<br>
 Behavior:<br>
   -<b>Right clicking</b> make showing/hiding all tracks except first, so it's perfect for watching some video in a foreign language, and in case of understanding context problems right clicking to see the translation.<br>
-  -The even tracks will be shown in the top of the video, and the odd in the bottom.<br>
+  -The maximum of simultaneous subtitles it is 2, more subtitles at the same time is possible, but not practical, so this is the config for now.<br>
+  -If you drag a .srt or .vtt subtitle file into a video, it will be loaded automatically. If there were 2 subtitles before the load, the last will be substituted.<br>
   -Only works in chrome for now.
--- multisub.js
+++ multisub.js
@@ -3,59 +3,175 @@
  */
 (function () {
     var videos;
-    var video;
-    var tracklist;
-    var track;
-    var cues;
-    var colors = [];
-    window.onload = function () {
+    document.addEventListener("DOMContentLoaded", function () {
         videos = document.querySelectorAll("video[data-multisub]");
         for (var i = 0; i < videos.length; i++) {
-            video = videos[i];
-            colors = video.dataset.multisub.split(",");
+            var video = videos[i];
+            video.addEventListener(
+                'dragenter'
+                ,
+                function (e) {
+                    e.preventDefault();
+                }, false)
+            ;
+            video.addEventListener(
+                'dragover'
+                ,
+                function (e) {
+                    e.preventDefault();
+                }, false)
+            ;
+            video.addEventListener(
+                'drop'
+                , function (e) {
+                    soltado(e, this);
+                },
+                false)
+            ;
             for (var j = 0; j < video.textTracks.length; j++) {
-                video.textTracks[j].mode = "hidden";
-            }
-            var start = function () {
-                updateCues();
-            };
-            video.addEventListener("play", start);
-        }
-        function updateCues() {
-            video.pause();
-            tracklist = video.textTracks;
-            for (var i = 0; i < tracklist.length; i++) {
-                track = tracklist[i];
-                cues = track.cues;
-                for (var j = 0; j < cues.length; j++) {
-                    if ((i + 1) % 2 == 0) {
-                        cues[j].line = 1;
-                    }
-                    cues[j].text = "<c." + colors[i] + ">" + cues[j].text + "</c.>";
-                }
-                track.mode = "showing";
-            }
-            video.removeEventListener("play", start);
-            video.addEventListener('contextmenu', function (ev) {
-                ev.preventDefault();
-                hideshowCues();
-            });
-            video.play();
-        };
-        function hideshowCues() {
-            tracklist = video.textTracks;
-            for (var i = 0; i < tracklist.length; i++) {
-                if (i == 0) {
+                if (j > 1) {
+                    var tracktoremove = video.querySelectorAll("track")[j];
+                    video.removeChild(tracktoremove);
                     continue;
                 }
-                track = tracklist[ij];
-                if (track.mode == "showing") {
-                    track.mode = "hidden";
+                video.textTracks[j].mode = "hidden";
+            }
+            var attr = document.createAttribute("data-subnum");
+            attr.value = 0;
+            video.setAttributeNode(attr);
+            video.addEventListener("play", function (e) {
+                e.target.removeEventListener(e.type, arguments.callee);
+                return prepareTracks(this, true);
+            });
+        }
+    });
+    function prepareTracks(video, listener) {
+        var colors = video.dataset.multisub.split(",");
+        var tracklist;
+        var track;
+        var cues;
+        video.pause();
+        tracklist = video.textTracks;
+        for (var i = 0; i < tracklist.length; i++) {
+            track = tracklist[i];
+            //track.mode = "hidden";
+            cues = track.cues;
+            for (var j = 0; j < cues.length; j++) {
+                if (i == 1) {
+                    cues[j].line = 1;
                 } else {
-                    video.pause();
-                    track.mode = "showing";
+                    cues[j].line = "auto";
+                }
+                if (cues[j].text.match(/<c.[a-z]+>/)) {
+                    cues[j].text = cues[j].text.replace(/<c.[a-z]+>/, "<c." + colors[i] + ">");
+                } else {
+                    cues[j].text = "\<c." + colors[i] + ">" + cues[j].text + "\</c>";
                 }
             }
+            track.mode = "showing";
         }
-    };
+        if (listener == true) {
+            video.addEventListener('contextmenu', function (ev) {
+                ev.preventDefault();
+                hideshowCues(this);
+            });
+        }
+        video.play();
+    }
+
+    function hideshowCues(video) {
+        var tracklist;
+        var track;
+        tracklist = video.textTracks;
+        for (var i = 0; i < tracklist.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            track = tracklist[i];
+            if (track.mode == "showing") {
+                track.mode = "hidden";
+            } else {
+                video.pause();
+                track.mode = "showing";
+            }
+        }
+    }
+
+    function soltado(e, video) {
+        e = e || window.event;
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        var dt = e.dataTransfer;
+        var files = dt.files;
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            dragLoad(file, video);
+        }
+        return false;
+    }
+
+    function dragLoad(file, video) {
+        var track;
+        var parts = [];
+        var ini = [];
+        var end = [];
+        var type = file.name.split('.').pop();
+        if (type == "srt" || type == "vtt") {
+            video.pause();
+            var regex;
+            if (type == "srt") {
+                regex = /[0-9]+:[0-9]+:[0-9]+,[0-9]+\s-->\s[0-9]+:[0-9]+:[0-9]+,[0-9]+/g;
+            } else {
+                regex = /[0-9]+:[0-9]+:[0-9]+.[0-9]+\s-->\s[0-9]+:[0-9]+:[0-9]+.[0-9]+/g
+            }
+            if (video.textTracks.length == 2) {
+                track = video.textTracks[video.dataset.subnum % 2];
+                video.dataset.subnum++;
+                track.src = "";
+                track.mode = "hidden";
+                while (track.cues.length > 0) {
+                    track.removeCue(track.cues[0]);
+                }
+            } else {
+                video.addTextTrack("subtitles");
+                var x = video.textTracks.length - 1;
+                track = video.textTracks[x];
+                track.mode = "hidden";
+            }
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var txt = e.target.result.split("\n", -1);
+                var texto = "";
+                for (var i = 0; i < txt.length; i++) {
+                    if (!isNaN(txt[i])) {
+                        continue;
+                    }
+                    if (txt[i].match(regex)) {
+                        var linea = txt[i].replace(/,/g, ".");
+                        parts = linea.split("-->");
+                        ini = parts[0].split(":");
+                        end = parts[1].split(":");
+                        continue;
+                    }
+                    if (txt[i]) {
+                        if (texto) {
+                            texto += "<br>" + txt[i];
+                        } else {
+                            texto += txt[i];
+                        }
+                        if (txt[i + 1] == "\r" || txt[i + 1] == "") {
+                            track.addCue(new VTTCue((parseFloat(ini[0] * 3600) + parseFloat(ini[1] * 60) + parseFloat(ini[2])), (parseFloat(end[0] * 3600) + parseFloat(end[1] * 60) + parseFloat(end[2])), texto));
+                            texto = "";
+                        }
+                    }
+                }
+                prepareTracks(video, false);
+            }
+            ;
+            reader.readAsText(file);
+        } else {
+            console.log("mal archivo");
+        }
+    }
 })();
